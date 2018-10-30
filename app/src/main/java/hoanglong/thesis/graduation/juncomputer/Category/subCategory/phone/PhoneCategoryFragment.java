a3d1b937bb97e30d6962f546395674a493f45a1a
tran hoang.longb <tran.hoang.longb@framgia.com>
2018-10-30 11:45:14 +0700

    Category

---
+++ app/src/main/java/hoanglong/thesis/graduation/juncomputer/Category/subCategory/phone/PhoneCategoryFragment.java
@@ -0,0 +1,30 @@
+package hoanglong.thesis.graduation.juncomputer.Category.subCategory.phone;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hoanglong.thesis.graduation.juncomputer.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PhoneCategoryFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_phone_category, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
--- app/src/main/java/hoanglong/thesis/graduation/juncomputer/Utils/Constant.java
+++
@@ -1,5 +0,0 @@
-package hoanglong.thesis.graduation.juncomputer.Utils;
-
-public class Constant {
-    public static final String URL = "https://myshoppingver1.herokuapp.com/getAllCategoryPhone";
-}
